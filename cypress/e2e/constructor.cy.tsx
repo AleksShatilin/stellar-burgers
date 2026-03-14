332dc83a5545a3a9d967694f034cfd11d0c41f18
Алексей <IIGamerIISaf@yandex.ru>
2026-03-15 02:00:32 +0300

    Исправления по замечаниям ревьюера
    - Исправлены Cypress-тесты (теперь 6/6 проходят)
    - Повторяющиеся строки вынесены в константы
    - Повторяющиеся объекты вынесены в константы
    - Добавлены ожидания перед поиском элементов
    - Исправлены перехваты запросов

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,10 +1,15 @@
 /// <reference types="cypress" />
 
+const BUN_NAME = 'Краторная булка N-200i';
+const MAIN_NAME = 'Биокотлета из марсианской Магнолии';
+const MODAL_TITLE = 'Детали ингредиента';
+
 describe('Страница конструктора', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as('getIngredients');
     cy.visit('/');
     cy.wait('@getIngredients');
+    cy.contains(BUN_NAME, { timeout: 10000 }).should('be.visible');
   });
 
   it('должна загружаться', () => {
@@ -12,49 +17,49 @@ describe('Страница конструктора', () => {
   });
 
   it('должен добавлять булку в конструктор', () => {
-    cy.contains('Краторная булка N-200i').parent().parent().contains('Добавить').click();
+    cy.contains(BUN_NAME).parent().parent().contains('Добавить').click();
     cy.get('.constructor-element').should('have.length', 2);
   });
 
   it('открывает модальное окно ингредиента при клике на карточку', () => {
-    cy.contains('Краторная булка N-200i').click();
-    cy.contains('Детали ингредиента').should('be.visible');
+    cy.contains(BUN_NAME).click();
+    cy.contains(MODAL_TITLE).should('be.visible');
   });
 
   it('закрывает модальное окно по клику на крестик', () => {
-    cy.contains('Краторная булка N-200i').click();
-    cy.contains('Детали ингредиента').should('be.visible');
-    cy.contains('Детали ингредиента').parent().find('button').click();
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains(BUN_NAME).click();
+    cy.contains(MODAL_TITLE).should('be.visible');
+    cy.contains(MODAL_TITLE).parent().find('button').click();
+    cy.contains(MODAL_TITLE).should('not.exist');
   });
 
   it('закрывает модальное окно по клику на оверлей', () => {
-    cy.contains('Краторная булка N-200i').click();
-    cy.contains('Детали ингредиента').should('be.visible');
+    cy.contains(BUN_NAME).click();
+    cy.contains(MODAL_TITLE).should('be.visible');
     cy.get('body').click(0, 0);
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains(MODAL_TITLE).should('not.exist');
   });
 
 it('должен создавать заказ', () => {
-  cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' }).as('getUser');
-  cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as('createOrder');
+  cy.intercept('GET', '**/auth/user', { fixture: 'user.json' }).as('getUser');
+  cy.intercept('POST', '**/orders', { fixture: 'order.json' }).as('createOrder');
 
   cy.window().then((win) => {
     win.localStorage.setItem('accessToken', 'mock-token');
     win.localStorage.setItem('refreshToken', 'mock-token');
   });
 
-  cy.contains('Краторная булка N-200i').parent().parent().contains('Добавить').click();
-  cy.contains('Биокотлета из марсианской Магнолии').parent().parent().contains('Добавить').click();
-  cy.contains('Оформить заказ').click();
-  cy.wait('@createOrder');
+  cy.wait('@getUser', { timeout: 10000 });
 
-  // Проверяем, что номер заказа отображается
+  cy.contains(BUN_NAME).parent().parent().contains('Добавить').click();
+  cy.contains(MAIN_NAME).parent().parent().contains('Добавить').click();
+
+  cy.contains('Оформить заказ').should('be.enabled').click();
+
+  cy.wait('@createOrder', { timeout: 10000 });
+
   cy.contains('12345').should('be.visible');
-
-  // Закрываем модалку кликом по оверлею
   cy.get('body').click(0, 0);
-
   cy.get('.constructor-element').should('have.length', 0);
 });
 });
